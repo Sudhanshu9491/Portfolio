64589b03c7e4f322fe314fb8a88af679247a8a0f
Sudhanshu Mishra <148893552+Sudhanshu9491@users.noreply.github.com>
2024-05-22 12:28:12 +0530

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -16,31 +16,31 @@ const mywork_data = [
         w_no:2,
         w_name:"Web design",
         w_img:project2_img,
-        link:"https://sudhanshu9491.github.io/Ecommerce_webpage/"
+        link:"https://amazon-clone9491.vercel.app/"
     },
     {
         w_no:3,
         w_name:"Web design",
         w_img:project3_img,
-        link:"https://sudhanshu9491.github.io/Ecommerce_webpage/"
+        link:"https://spotify-clone9491.vercel.app/"
     },
     {
         w_no:4,
         w_name:"Web design",
         w_img:project4_img,
-        link:"https://sudhanshu9491.github.io/Ecommerce_webpage/"
+        link:"https://college-registration9491.vercel.app"
     },
     {
         w_no:5,
         w_name:"Web design",
         w_img:project5_img,
-        link:"https://sudhanshu9491.github.io/Ecommerce_webpage/"
+        link:"https://weather-wise9491.vercel.app/"
     },
     {
         w_no:6,
         w_name:"Web design",
         w_img:project6_img,
-        link:"https://sudhanshu9491.github.io/Ecommerce_webpage/"
+        link:"#"
     },
 ]
  
